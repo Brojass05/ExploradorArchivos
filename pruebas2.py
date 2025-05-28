dff782879b47ee0cca7f57e9f86699f2d1cfba0a
Benjamin Rojas <144973521+Brojass05@users.noreply.github.com>
2025-05-28 13:16:35 -0400

    Continuacion get_archivos
    se continua la obtencion de archivos por directorios pero aun no se logra

--- pruebas2.py
+++ pruebas2.py
@@ -13,18 +13,28 @@ partes_ruta = [x for x in ruta_actual.split('\\') if x != '']
 ruta_anterior: str = ruta_actual.split(partes_ruta[-1])[0].rstrip('\\')
 print(ruta_anterior)
 # crea listas para las rutas y los nombres de los archivos
-listaRutas: list = []
-nombreArchivos: list = []
-# ciclo for para ir agregando las rutas y los nombres a sus listas respectivas
-for i in range(0,len(archivos_en_ruta)):
-    # agrega la ruta a la lista desde C:, hasta la ruta actual
-    listaRutas.append(os.path.join(ruta_anterior, str(archivos_en_ruta[i])))
-    # agrega los nombres de los archivos dentro de la ruta actual
-    nombreArchivos.append(archivos_en_ruta[i])
+
+
 listas_rutas_anteriores: list = []    
 for n in range(1, len(partes_ruta)):
     ruta = ruta_actual.split(partes_ruta[-n])[0].rstrip('\\')
     listas_rutas_anteriores.append(ruta)
-    
-for rutas in listas_rutas_anteriores:
+
+listaRutas: list = []
+nombreArchivos: list = []
+for ruta in listas_rutas_anteriores:
+    # ciclo for para ir agregando las rutas y los nombres a sus listas respectivas
+    print("Ruta1: ",ruta)
+    listaRutas.append(os.path.join(ruta))
+    for i in range(0,len(listas_rutas_anteriores)):
+        # agrega la ruta a la lista desde C:, hasta la ruta actual
+        listaRutas.append(os.path.join(ruta_anterior, str(listas_rutas_anteriores[i])))
+        # agrega los nombres de los archivos dentro de la ruta actual
+        nombreArchivos.append(listas_rutas_anteriores[i])
+        print("Agregando: ",listas_rutas_anteriores[i])
+for rutas in listaRutas:
     print(rutas)
+
+
+
+    
